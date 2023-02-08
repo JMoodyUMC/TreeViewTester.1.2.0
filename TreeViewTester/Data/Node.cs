c478c5c2a1657fd8eff986ac796643d39ed6bf3e
JMoody <JMoody@CS1601>
2023-02-08 09:07:06 -0500

    Add project files.

---
+++ TreeViewTester/Data/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TreeViewTester.Data
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Type { get; set; }
+        public List<Node> Children { get; set; }
+        public string? ParentID { get; set; }
+
+        public Node(Int64 nId, string sType, string sText)
+        {
+            Id = String.Format("{0}{1}", sType, nId);
+            Type = sType;
+            Text = sText;
+            Children = new List<Node>();
+        }
+
+        public void AddChild(Node child)
+        {
+            child.ParentID = Id;
+            Children.Add(child);
+        }
+    }
+}
---
+++ TreeViewTester/Data/TimeCard.cs
@@ -0,0 +1,18 @@
+﻿namespace TreeViewTester.Data
+{
+    public class TimeCard
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public DateTime CardDate { get; set; }
+
+        public TimeCard(int id, int employeeId, string name, DateTime cardDate)
+        {
+            Id = id;
+            EmployeeId = employeeId;
+            Name = name;
+            CardDate = cardDate;
+        }
+    }
+}
